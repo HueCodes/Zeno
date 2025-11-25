c66a123f358b04de64a12fc9f3e2561b805112fe
Hugh <Hugh.t.Ryan@gmail.com>
2025-11-24 17:22:16 -0800

    Run initial reconcile immediately

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -35,6 +35,11 @@ func New(cfg *config.Config) (*Controller, error) {
 func (c *Controller) Run(ctx context.Context) error {
 	log.Println("controller starting...")
 
+	// Run initial reconcile immediately instead of waiting for first tick
+	if err := c.reconcile(ctx); err != nil {
+		log.Printf("initial reconcile error: %v", err)
+	}
+
 	ticker := time.NewTicker(c.cfg.Runner.CheckInterval)
 	defer ticker.Stop()
 
